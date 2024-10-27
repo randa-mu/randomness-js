577f698710396a7bede8c7f8fc09574631274fa2
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-10-27 17:30:17 +0100

    updated interface for RandomnessReceiver to provide the signature

--- contracts/Randomness.sol
+++ contracts/Randomness.sol
@@ -2,12 +2,34 @@
 pragma solidity 0.8.24;
 
 import {FeistelShuffleOptimised} from "./FeistelShuffleOptimised.sol";
+
 library Randomness {
-  
+
+    /**
+      * @notice takes a signature from the drand or dcrypt network and returns a uniform set of random bytes
+      * @dev It is intended to be called with a valid signature from the dcrypt or drand networks, but can also be called
+      * with random bytes from another source.
+      * @param signature is a valid signature from the dcrypt or drand networks
+      */
+    function fromSignature(bytes memory signature)
+    public
+    pure
+    returns (bytes32) {
+        return keccak256(bytes);
+    }
+
+    /**
+      * @notice selects a set of indices randomly from a given array length
+      * @dev It is intended to be called with a valid signature from the dcrypt or drand networks, but can also be called
+      * with random bytes from another source.
+      * @param lengthOfArray the length of the array you wish to draw indices from
+      * @param countToDraw the number of indices you wish to draw from the array
+      * @param signature the signature used to derive randomness
+      */
     function selectArrayIndices(uint256 lengthOfArray, uint256 countToDraw, bytes calldata signature)
-        public
-        pure
-        returns (uint256[] memory)
+    public
+    pure
+    returns (uint256[] memory)
     {
         if (lengthOfArray == 0) {
             return new uint256[](0);
@@ -21,7 +43,7 @@ library Randomness {
             return winners;
         }
 
-        bytes32 randomBytes = keccak256(signature);
+        bytes32 randomBytes = fromSignature(signature);
         uint256 randomness;
         assembly {
             randomness := randomBytes
