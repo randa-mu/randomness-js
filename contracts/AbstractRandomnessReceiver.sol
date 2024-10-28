b8a8f46b1a56399bada753c1f3d863f2518c3c80
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-10-28 13:48:21 +0100

    feat: AbstractRandomnessReceiver.sol

---
+++ contracts/AbstractRandomnessReceiver.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IRandomnessReceiver} from "./IRandomnessReceiver.sol";
+
+abstract contract AbstractRandomnessReceiver {
+    address randomnessRequester;
+
+    error NotAuthorizedRandomnessProvider();
+
+    modifier onlyRandomnessProvider(){
+        if (msg.sender != randomnessRequester) revert NotAuthorizedRandomnessProvider();
+        _;
+    }
+
+    constructor(address _randomnessRequester) public {
+        randomnessRequester = _randomnessRequester;
+    }
+}
