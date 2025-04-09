139a25f390099064787829c1d50d6b713626bfee
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-04-10 00:32:53 +0200

    removed mcl-wasm and added noble
    - added solidity as a submodule
    - generate code on build
    - created a better integration test with params from github secrets
    - created helpers for instantiating `Randomness`

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A convenience library for retrieving, verifying and deriving randomness from the
 Build everything by running `npm run build`. This creates a dist directory containing commonjs and esm modules.
 
 Solidity interfaces for randomness can be found in our [solidity repo](https://github.com/randa-mu/randamu-solidity-contracts).
+
+## Development
+- Clone the repo
+- run `git submodule update --init --recursive`
+For running the tests, you need to create a `.env` file at the project root filling in the fields detailed in [`.env.sample`](./.env.sample).
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import {Buffer} from "buffer"
 import {
     BytesLike,
     getBytes,
@@ -6,26 +5,23 @@ import {
     Provider,
     Signer,
 } from "ethers"
-import {G2} from "mcl-wasm"
-import {RandomnessSender__factory} from "./generated"
-import {
-    RandomnessCallbackFailedEvent,
-    RandomnessCallbackSuccessEvent,
-    RandomnessSender
-} from "./generated/RandomnessSender"
-import {BlsBn254, bytesEqual} from "./bls-bn254"
+import {bn254} from "@kevincharm/noble-bn254-drand"
+import {equalBytes} from "@noble/curves/abstract/utils"
 import {encodeParams, extractSingleLog} from "./ethers-helpers"
 import {withTimeout} from "./misc"
+import {RandomnessSender__factory} from "./generated"
 import {TypedContractEvent, TypedListener} from "./generated/common"
+import {RandomnessCallbackSuccessEvent, RandomnessSender} from "./generated/RandomnessSender"
 
-const defaultContractAddress = "0x9ed5a27a9f18529848d03ae699bdecc2ba8d10fe"
-// the verifier's BLS public key arranged as magical bigints to avoid promises in creation
-export const VERIFIER_PUBLIC_KEY: [bigint, bigint, bigint, bigint] = [
-    17445541620214498517833872661220947475697073327136585274784354247720096233162n,
-    18268991875563357240413244408004758684187086817233527689475815128036446189503n,
-    11401601170172090472795479479864222172123705188644469125048759621824127399516n,
-    8044854403167346152897273335539146380878155193886184396711544300199836788154n,
-]
+/* addresses of the deployed contracts */
+export const FURNACE_TESTNET_CONTRACT_ADDRESS = "0x6ac6390653d8d75b9baff66401198efc3684e0ec"
+export const FILECOIN_CALIBNET_CONTRACT_ADDRESS = "0x9c789bc7F2B5c6619Be1572A39F2C3d6f33001dC"
+
+/* some cryptographic parameters that are also defined in the contracts, but we duplicate here for performance */
+const RANDOMNESS_DST = "randomness:0.0.1:bn254"
+const BLS_DST = "BLS_SIG_BN254G1_XMD:KECCAK-256_SVDW_RO_NUL_"
+
+/* ethers.js magic beans */
 const iface = RandomnessSender__factory.createInterface()
 
 export type RandomnessVerificationParameters = {
@@ -37,12 +33,21 @@ export type RandomnessVerificationParameters = {
 
 export class Randomness {
     private readonly contract: RandomnessSender
-    private bls: BlsBn254 | undefined
-    private pk: G2 | undefined
+    // the format of this public key is _NOT_ the same format as evm-land.
+    // only the lord himself knows how to convert from one to the other
+    private readonly pk = bn254.G2.ProjectivePoint.fromHex("00f26a1fbba69685a1606f8104d0f218546a774099d78218e01bf63bf08b94fb0eafa51a62570209e04f66f390f41b7a1dae3d9350af7b413e4c65ffc4ca3a6c0815fe3e100c6f2c9a05f8ac898f9aa5c54164771500426ce54b52c6e0958e52111fa5a435e9d442cf69939a379e25841c65c3be365e851fdd04539e9b2462a1")
 
-    constructor(private readonly rpc: Signer | Provider, private readonly contractAddress: string = defaultContractAddress) {
+    constructor(private readonly rpc: Signer | Provider, private readonly contractAddress: string = FURNACE_TESTNET_CONTRACT_ADDRESS) {
         console.log(`created randomness-js client with address ${contractAddress}`)
         this.contract = RandomnessSender__factory.connect(contractAddress, rpc)
+    }
+
+    static createFilecoinCalibnet(rpc: Signer | Provider): Randomness {
+        return new Randomness(rpc, FILECOIN_CALIBNET_CONTRACT_ADDRESS)
+    }
+
+    static createFurnace(rpc: Signer | Provider): Randomness {
+        return new Randomness(rpc, FURNACE_TESTNET_CONTRACT_ADDRESS)
     }
 
     async requestRandomness(confirmations = 1, timeoutMs = 60000): Promise<RandomnessVerificationParameters> {
@@ -65,7 +70,6 @@ export class Randomness {
             // lest we miss our fulfilled randomness. We set the listeners first... in case
             // by some magic our request is fulfilled _between_ the lines
             const successFilter = this.contract.filters.RandomnessCallbackSuccess(requestID)
-            const failureFilter = this.contract.filters.RandomnessCallbackFailed(requestID)
 
             // how many blocks we're willing to look back - don't really expect it to be more than 1
             const blockLookBack = 3
@@ -73,7 +77,6 @@ export class Randomness {
             // cleanup to do once we've managed all the events
             const cleanup = () => {
                 this.contract.off(successFilter)
-                this.contract.off(failureFilter)
             }
             const randomnessCallback = (result: RandomnessVerificationParameters) => {
                 resolve(result)
@@ -81,13 +84,9 @@ export class Randomness {
             }
             const randomnessListener = createRandomnessListener(nonce, randomnessCallback)
             this.contract.once(successFilter, randomnessListener)
-            this.contract.once(failureFilter, randomnessListener)
 
             const randomnessFilter = createRandomnessLogListener(nonce, randomnessCallback)
             this.contract.queryFilter(successFilter, -blockLookBack)
-                .then(randomnessFilter)
-                .catch(reject)
-            this.contract.queryFilter(failureFilter, -blockLookBack)
                 .then(randomnessFilter)
                 .catch(reject)
 
@@ -99,37 +98,26 @@ export class Randomness {
     }
 
     async verify(parameters: RandomnessVerificationParameters): Promise<boolean> {
-        if (!this.bls) {
-            this.bls = await BlsBn254.create()
-        }
-        if (!this.pk) {
-            this.pk = this.bls.g2FromEvm(VERIFIER_PUBLIC_KEY)
-        }
         const {randomness, signature, nonce} = parameters
-        const randDST = "randomness:0.0.1:bn254"
-        const blsDST = Buffer.from("BLS_SIG_BN254G1_XMD:KECCAK-256_SVDW_RO_NUL_")
 
-        if (!bytesEqual(keccak256(signature), randomness)) {
+        if (!equalBytes(getBytes(keccak256(signature)), getBytes(randomness))) {
             throw Error("randomness did not match the signature provided")
         }
 
-        const m = keccak256(encodeParams(["string", "uint256"], [randDST, nonce]))
-        const h_m = this.bls.hashToPoint(blsDST, getBytes(m))
-        const s = this.bls.g1FromEvmHex(signature)
-
-        return this.bls.verify(h_m, this.pk, s)
+        const m = keccak256(encodeParams(["string", "uint256"], [RANDOMNESS_DST, nonce]))
+        return bn254.verifyShortSignature(getBytes(signature), getBytes(m), this.pk, {DST: BLS_DST})
     }
 }
 
 function createRandomnessListener(nonce: bigint, cb: (arg: RandomnessVerificationParameters) => void): TypedListener<TypedContractEvent> {
-    return (log: RandomnessCallbackSuccessEvent.Log | RandomnessCallbackFailedEvent.Log) => {
+    return (log: RandomnessCallbackSuccessEvent.Log) => {
         const [requestID, randomness, signature] = log.args
         cb({requestID, nonce, randomness, signature})
     }
 }
 
 function createRandomnessLogListener(nonce: bigint, cb: (arg: RandomnessVerificationParameters) => void) {
-    return (logs: RandomnessCallbackSuccessEvent.Log[] | RandomnessCallbackFailedEvent.Log[]) => {
+    return (logs: RandomnessCallbackSuccessEvent.Log[]) => {
         if (logs.length === 0) {
             return
         }
@@ -141,5 +129,3 @@ function createRandomnessLogListener(nonce: bigint, cb: (arg: RandomnessVerifica
         cb({requestID, randomness, signature, nonce})
     }
 }
-
-
