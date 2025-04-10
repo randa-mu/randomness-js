9f3e5f9e8313595ca11d014bc15e31e1a744ac63
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-04-10 18:06:07 +0200

    added tests for base sepolia

--- test/randomness.test.ts
+++ test/randomness.test.ts
@@ -1,7 +1,12 @@
 import * as dotenv from "dotenv"
 import {describe, it, expect, beforeAll} from "@jest/globals"
 import {JsonRpcProvider, NonceManager, Wallet, WebSocketProvider} from "ethers"
-import {FILECOIN_CALIBNET_CONTRACT_ADDRESS, FURNACE_TESTNET_CONTRACT_ADDRESS, Randomness} from "../src"
+import {
+    BASE_SEPOLIA_CONTRACT_ADDRESS,
+    FILECOIN_CALIBNET_CONTRACT_ADDRESS,
+    FURNACE_TESTNET_CONTRACT_ADDRESS,
+    Randomness
+} from "../src"
 
 // filecoin calibnet might take forever
 const TEST_TIMEOUT = 200_000
@@ -9,8 +14,8 @@ describe("randomness", () => {
     beforeAll(() => {
         dotenv.config()
     })
-
-    it("can be requested from filecoin testnet and verified", async () => {
+    
+      it("can be requested from filecoin testnet and verified", async () => {
         const rpc = createProvider(process.env.FILECOIN_RPC_URL || "")
         const wallet = new NonceManager(new Wallet(process.env.FILECOIN_PRIVATE_KEY || "", rpc))
 
@@ -28,6 +33,20 @@ describe("randomness", () => {
         const wallet = new NonceManager(new Wallet(process.env.FURNACE_PRIVATE_KEY || "", rpc))
 
         const randomness = new Randomness(wallet, FURNACE_TESTNET_CONTRACT_ADDRESS)
+        expect(randomness).not.toEqual(null)
+
+        const response = await randomness.requestRandomness(1, TEST_TIMEOUT)
+        console.log("randomness requested")
+        await randomness.verify(response)
+
+        rpc.destroy()
+    }, TEST_TIMEOUT)
+
+    it("can be requested from a base sepolia and verified", async () => {
+        const rpc = createProvider(process.env.BASE_RPC_URL || "")
+        const wallet = new NonceManager(new Wallet(process.env.BASE_PRIVATE_KEY || "", rpc))
+
+        const randomness = new Randomness(wallet, BASE_SEPOLIA_CONTRACT_ADDRESS)
         expect(randomness).not.toEqual(null)
 
         const response = await randomness.requestRandomness(1, TEST_TIMEOUT)
