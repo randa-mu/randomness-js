cb3be33b3210d0d97a25816a7e9e200cced4394b
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-04-10 21:34:15 +0200

    bumped version

--- test/randomness.test.ts
+++ test/randomness.test.ts
@@ -15,19 +15,6 @@ describe("randomness", () => {
         dotenv.config()
     })
 
-      it("can be requested from filecoin testnet and verified", async () => {
-        const rpc = createProvider(process.env.FILECOIN_RPC_URL || "")
-        const wallet = new NonceManager(new Wallet(process.env.FILECOIN_PRIVATE_KEY || "", rpc))
-
-        const randomness = new Randomness(wallet, FILECOIN_CALIBNET_CONTRACT_ADDRESS)
-        expect(randomness).not.toEqual(null)
-
-        const response = await randomness.requestRandomness(1, TEST_TIMEOUT)
-        await randomness.verify(response)
-
-        rpc.destroy()
-    }, TEST_TIMEOUT)
-
     it("can be requested from a furnace testnet and verified", async () => {
         const rpc = createProvider(process.env.FURNACE_RPC_URL || "")
         const wallet = new NonceManager(new Wallet(process.env.FURNACE_PRIVATE_KEY || "", rpc))
@@ -70,14 +57,27 @@ describe("randomness", () => {
         rpc.destroy()
     }, TEST_TIMEOUT)
 
+    it("can be requested from filecoin testnet and verified", async () => {
+        const rpc = createProvider(process.env.FILECOIN_RPC_URL || "")
+        const wallet = new NonceManager(new Wallet(process.env.FILECOIN_PRIVATE_KEY || "", rpc))
+
+        const randomness = new Randomness(wallet, FILECOIN_CALIBNET_CONTRACT_ADDRESS)
+        expect(randomness).not.toEqual(null)
+
+        const response = await randomness.requestRandomness(1, TEST_TIMEOUT)
+        await randomness.verify(response)
+
+        rpc.destroy()
+    }, TEST_TIMEOUT)
+
 })
 
 function createProvider(url: string): JsonRpcProvider | WebSocketProvider {
     if (url.startsWith("http")) {
-        return new JsonRpcProvider(url, undefined, { pollingInterval: 1000 })
+        return new JsonRpcProvider(url, undefined, {pollingInterval: 1000})
     }
     if (url.startsWith("ws")) {
-        return  new WebSocketProvider(url)
+        return new WebSocketProvider(url)
     }
     throw new Error(`provider cannot be created for the protocol in ${url}`)
 }
